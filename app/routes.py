ad80dd6c78de0f44d3d8f4f5f40f6b211bcfd037
Mukhtar <dagdahzub@mail.ru>
2018-02-27 15:39:58 +0300

    добавили последние изменения по сайту

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import render_template, request, redirect, url_for
 from app.models import Work
 from app.forms import WorkForm
@@ -7,7 +8,8 @@ from app import app, db
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('main.html')
+    last_works = Work.query.all()
+    return render_template('main.html', works=last_works)
 
 
 @app.route('/works', methods=['GET'])
@@ -30,6 +32,9 @@ def add_work():
     if form.validate_on_submit():
         work = Work()
         work.title = form.title.data
+        work.youtube_url = form.youtube_url.data
+        work.image_url = form.image_url.data
+        work.body = form.body.data
         db.session.add(work)
         db.session.commit()
         return redirect('/works')
@@ -44,3 +49,20 @@ def test_page():
 @app.route('/qa')
 def qa_page():
     return render_template('qa_page.html')
+
+
+@app.route('/upload_insta_photo')
+def upload_insta_photo():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    photo_path = base_dir + '/static/img/content/work6.jpg'
+    # from InstagramAPI import InstagramAPI
+    # InstagramAPI = InstagramAPI("dagstyazhka", "gmayunus2018")
+    # InstagramAPI.login()
+    # caption = "Выполним полусухую стяжку пола быстро и качественно"
+    # InstagramAPI.uploadPhoto(photo_path, caption=caption)
+    return 'Photo uploaded' + photo_path
+
+
+@app.route('/insta/images')
+def show_images_insta():
+    return 'Images from insta'
